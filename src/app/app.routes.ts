90aad00c7b37f535c79e61dd799f8cb03b812dfd
singhps18 <singh1998priya18@gmail.com>
2025-05-29 00:42:56 +0530

    add user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 export const routes: Routes = [
     {path:"",redirectTo:"dashboard",pathMatch:"full"},
     {path:'dashboard',component:DashboardComponent},
     {
         path:'gallery',component:GalleryComponent
+    },
+    {
+        path:'users', component:UserFormComponent
     }
 ];
---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatModule } from '../../../matmodule';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [MatModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.scss'
+})
+export class UserFormComponent {
+
+}
